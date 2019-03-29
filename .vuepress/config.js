c52a54a6aac2810e79f7ed53da36ea67d8f9d901
Demo Macro <abc@imst.xyz>
2019-03-29 13:13:52 +0800

    Update config.js

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -39,7 +39,7 @@ module.exports = {
 
 
     // 假定是 GitHub. 同时也可以是一个完整的 GitLab URL
-    repo: 'DemoMacro/VueCMS',
+    repo: 'DemoMacro/VuePress-NetlifyCMS',
     // 自定义仓库链接文字。默认从 `themeConfig.repo` 中自动推断为
     // "GitHub"/"GitLab"/"Bitbucket" 其中之一，或是 "Source"。
     repoLabel: 'Github',
